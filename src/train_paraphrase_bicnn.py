9683500b0ce5b1e6e75ef51434f5f028c790704d
yinwenpeng <mr.yinwenpeng@gmail.com>
2015-10-27 18:14:05 +0100

    two layers emb reach 25.73%@epoch20

--- src/train_paraphrase_bicnn.py
+++ src/train_paraphrase_bicnn.py
@@ -24,10 +24,12 @@ from common_functions import Conv_with_input_para, Average_Pooling_for_batch1, c
 
 
 
-def evaluate_lenet5(learning_rate=0.08, n_epochs=2000, nkerns=[50], batch_size=1, window_width=3,
+def evaluate_lenet5(learning_rate=0.085, n_epochs=2000, nkerns=[50], batch_size=1, window_width=3,
                     maxSentLength=60, emb_size=300, hidden_size=200,
-                    margin=0.5, L2_weight=0.00001, update_freq=10):
+                    margin=0.5, L2_weight=0.00005, update_freq=10):
 
+    model_options = locals().copy()
+    print "model options", model_options
     rootPath='/mounts/data/proj/wenpeng/Dataset/MicrosoftParaphrase/tokenized_msr/';
     rng = numpy.random.RandomState(23455)
     datasets, vocab_size=load_msr_corpus(rootPath+'vocab.txt', rootPath+'tokenized_train.txt', rootPath+'tokenized_test.txt', maxSentLength)
